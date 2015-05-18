47ecd16626afb3ea1b8b8ea48c22760c334adf0e
Igal Tabachnik <hmemcpy@gmail.com>
2015-05-18 15:42:58 +0300

    Final rename of the host manifest (can't have 2 files named manifest.json)
    Setting up the id assigned by the chrome web store

--- src/host/register-host.bat
+++ src/host/register-host.bat
@@ -1,1 +1,1 @@
-REG ADD "HKCU\Software\Google\Chrome\NativeMessagingHosts\com.hmemcpy.chrome.regjump" /ve /t REG_SZ /d "%~dp0manifest.json" /f
+REG ADD "HKCU\Software\Google\Chrome\NativeMessagingHosts\com.hmemcpy.chrome.regjump" /ve /t REG_SZ /d "%~dp0nativehost.json" /f

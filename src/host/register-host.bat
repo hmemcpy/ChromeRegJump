c073c6356497175f536e36b2bd0937ae53c57711
botmtl <botmtl@gmail.com>
2016-01-21 09:31:48 -0500

    regjump.exe install from in register-host.bat

--- src/host/register-host.bat
+++ src/host/register-host.bat
@@ -1,1 +1,2 @@
 REG ADD "HKCU\Software\Google\Chrome\NativeMessagingHosts\com.hmemcpy.chrome.regjump" /ve /t REG_SZ /d "%~dp0nativehost.json" /f
+IF NOT EXIST .\regjump\regjump.exe @powershell.exe -ExecutionPolicy Bypass -File ".\Install-Regjump.ps1"
